27c16ec7720d4e1d083114259ce8d86a89bc53fb
grantrisk <riskgrant@gmail.com>
2022-04-23 16:13:52 -0400

    Added example folder

---
+++ PresentationExamples/ConsoleTest.cs
@@ -0,0 +1,14 @@
+
+namespace MyNamespace
+{
+    class MainClass
+    {
+        static void Main(string[] args){
+
+            Console.WriteLine("\nStack Emaple Output:"); 
+            StackExample.StackMethodExample();
+            Console.WriteLine('\n'); 
+
+        }
+    }
+}
---
+++ PresentationExamples/Stack.cs
@@ -0,0 +1,17 @@
+﻿namespace MyNamespace
+{
+    class StackExample
+    {
+        public static void StackMethodExample(){
+            Stack<int> myStack = new Stack<int>();
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            myStack.Push(4);
+
+            foreach (var item in myStack){
+                Console.Write(item + " "); 
+            }
+        }
+    }
+}
