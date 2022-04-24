a2957ddb49fd6bf7334214a1a474c70b6617d4b2
grantrisk <riskgrant@gmail.com>
2022-04-24 11:11:23 -0400

    Modified ConsoleTest

--- PresentationExamples/ConsoleTest.cs
+++ PresentationExamples/ConsoleTest.cs
@@ -5,45 +5,50 @@ namespace MyNamespace
     {
         static void Main(string[] args){
 
-            // ---------- Hello World Example ----------
+            // // ---------- Hello World Example ----------
             // Console.WriteLine("\nHello World Example Output:");
             // Console.WriteLine("Hello World!"); 
+            // Console.WriteLine('\n');
 
-            // ---------- Binary Search Example ----------
+            // // ---------- Binary Search Example ----------
             // Console.WriteLine("\nBinary Search Example Output:");
             // BinarySearch.initiateBinarySearch();
+            // Console.WriteLine('\n');
 
-            // ---------- Parameter Passing Example ----------
+            // // ---------- Parameter Passing Example ----------
             // Console.WriteLine("\nParameter Passing Example Output:");
             // ParameterPassing.staticPassing();
+            // Console.WriteLine('\n');
 
-            // ---------- Dictionary Example ----------
+            // // ---------- Dictionary Example ----------
             // Console.WriteLine("\nDictionary Example Output:"); 
             // Dictionary.initialize();
             // Console.WriteLine('\n'); 
 
-            // ---------- List Example ----------
+            // // ---------- List Example ----------
             // Console.WriteLine("\nList Example Output:"); 
             // Lists.initialize();
             // Console.WriteLine('\n'); 
 
-            // ---------- Queue Example ----------
+            // // ---------- Queue Example ----------
             // Console.WriteLine("\nQueue Example Output:"); 
             // QueueExample.QueueMethodExample();
             // Console.WriteLine('\n'); 
 
-            // ---------- LinkedList Example ----------
+            // // ---------- LinkedList Example ----------
             // Console.WriteLine("\nLinked List Example Output:"); 
             // LinkedListExample.LinkedListMethodExample();
             // Console.WriteLine('\n');
 
-            // ---------- Stack Example ----------
+            // // ---------- Stack Example ----------
             // Console.WriteLine("\nStack Example Output:"); 
             // StackExample.StackMethodExample();
+            // Console.WriteLine('\n');
 
-            // ---------- Merge Sort Example ----------
-            Console.WriteLine("\nMerge Sort Example Output:"); 
-            MergeSort.initialize();
+            // // ---------- Merge Sort Example ----------
+            // Console.WriteLine("\nMerge Sort Example Output:"); 
+            // MergeSort.initialize();
+            // Console.WriteLine('\n');
 
         }
     }
